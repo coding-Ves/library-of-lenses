9f14b0182f20a92b44edd4a69fd12256c897d140
Ves <veselynk@gmail.com>
2023-07-05 00:26:59 +0300

    chose: add constants

---
+++ src/common/constants.ts
@@ -0,0 +1,13 @@
+// Firebase configuration
+export const FIREBASE_API_KEY = 'AIzaSyBmRa0oABxSFs82G2kTNi_CC-KzqaS4HCY';
+export const FIREBASE_AUTH_DOMAIN = 'library-of-lenses.firebaseapp.com';
+export const FIREBASE_DATABASE_URL =
+    'https://library-of-lenses-default-rtdb.europe-west1.firebasedatabase.app/';
+export const FIREBASE_PROJECT_ID = 'library-of-lenses';
+export const FIREBASE_STORAGE_BUCKET = 'library-of-lenses.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '833994462337';
+export const FIREBASE_APP_ID = '1:833994462337:web:1d8a70c0350b563fda2980';
+
+// Dicebear HTML API
+export const DICEBEAR_AVATAR_BASE_URL =
+    'https://api.dicebear.com/6.x/fun-emoji/svg?seed=';
