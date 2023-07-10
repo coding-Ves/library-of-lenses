33b86b4609158575d089b22cedba020931087725
Ves <veselynk@gmail.com>
2023-07-10 21:59:05 +0300

    feat(constants): Add LENS_BOARD img urls

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -11,3 +11,13 @@ export const FIREBASE_APP_ID = '1:833994462337:web:1d8a70c0350b563fda2980';
 // Dicebear HTML API
 export const DICEBEAR_AVATAR_BASE_URL =
     'https://api.dicebear.com/6.x/fun-emoji/svg?seed=';
+
+// Lens Board Images
+export const LENS_BOARD_NAV_IMAGE =
+    'https://firebasestorage.googleapis.com/v0/b/library-of-lenses.appspot.com/o/images%2FLens%20image%20top%20raw.jpg?alt=media&token=96912b29-e29a-42e2-b736-8c1b4d8ecb89';
+export const LENS_BOARD_IMG_1 =
+    'https://firebasestorage.googleapis.com/v0/b/library-of-lenses.appspot.com/o/images%2FLens-image-top-raw_01.jpg?alt=media&token=f24f5a89-4b56-4e72-a435-35917faf35ed';
+export const LENS_BOARD_IMG_2 =
+    'https://firebasestorage.googleapis.com/v0/b/library-of-lenses.appspot.com/o/images%2FLens-image-top-raw_02.gif?alt=media&token=2c714012-9690-484c-bac5-d7429b7e5127';
+export const LENS_BOARD_IMG_3 =
+    'https://firebasestorage.googleapis.com/v0/b/library-of-lenses.appspot.com/o/images%2FLens-image-top-raw_03.gif?alt=media&token=1b82e866-dc1f-4bcc-880d-511e321e1b2f';
