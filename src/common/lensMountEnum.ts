13442cb7c0d648ae5ddd583a02500211cd4eeb1c
Ves <veselynk@gmail.com>
2023-07-15 01:11:37 +0300

    feat(enum): add LensMounts enum

---
+++ src/common/lensMountEnum.ts
@@ -0,0 +1,13 @@
+export enum LensMounts {
+    CANON_FD = 'Canon FD',
+    CONTAX_RF = 'Contax RF',
+    CONTAX_YASHICA = 'Contax/Yashica',
+    LEICA_R = 'Leica R',
+    LEICA_M = 'leica M',
+    M42 = 'm42',
+    M39 = 'm39 / LTM',
+    MINOLTA_MD = 'Minolta MD',
+    NIKON_F = 'Nikon F',
+    Pentax_K = 'Pentax K',
+    OLYMPUS_OM = 'Olympus OM',
+}
