6c2ca2221f50dabfe52220ea5ef8e43ebf167f4a
Ves <veselynk@gmail.com>
2023-07-27 01:14:53 +0300

    feat(services): add fetchPhotoById fn

--- src/services/flickr.service.ts
+++ src/services/flickr.service.ts
@@ -1,3 +1,5 @@
+import { FLICKR_API_KEY } from '../common/constants.ts';
+
 export const URLPhotoHandler = (url: string): string => {
     const splitUrl = url.split('/');
     return splitUrl[5];
@@ -6,4 +8,22 @@ export const URLPhotoHandler = (url: string): string => {
 export const URLAlbumHandler = (url: string): string => {
     const splitUrl = url.split('/');
     return splitUrl[6];
+};
+
+export const fetchPhotoById = (photoId: string) => {
+    const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${FLICKR_API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
+
+    const photo = fetch(apiUrl)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch photo');
+            }
+            const data = response.json();
+            return data;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    return photo;
 };
