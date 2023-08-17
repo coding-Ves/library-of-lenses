b77590b0ebb0bf245d67aea4f1a5dbc6b90a81f9
Ves <veselynk@gmail.com>
2023-08-17 21:30:58 +0300

    refactor(env): rework project to use .env file

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,14 +1,3 @@
-// Firebase configuration
-export const FIREBASE_API_KEY = 'AIzaSyBmRa0oABxSFs82G2kTNi_CC-KzqaS4HCY';
-export const FIREBASE_AUTH_DOMAIN = 'library-of-lenses.firebaseapp.com';
-export const FIREBASE_DATABASE_URL =
-    'https://library-of-lenses-default-rtdb.europe-west1.firebasedatabase.app/';
-export const FIREBASE_PROJECT_ID = 'library-of-lenses';
-export const FIREBASE_STORAGE_BUCKET = 'library-of-lenses.appspot.com';
-export const FIREBASE_MESSAGING_SENDER_ID = '833994462337';
-export const FIREBASE_APP_ID = '1:833994462337:web:1d8a70c0350b563fda2980';
-export const FLICKR_API_KEY = '90a7607572936b024247b50a87990e2b';
-
 // Dicebear HTML API
 export const DICEBEAR_AVATAR_BASE_URL =
     'https://api.dicebear.com/6.x/fun-emoji/svg?seed=';
@@ -22,3 +11,33 @@ export const LENS_BOARD_IMG_2 =
     'https://firebasestorage.googleapis.com/v0/b/library-of-lenses.appspot.com/o/images%2FLens-image-top-raw_02.gif?alt=media&token=2c714012-9690-484c-bac5-d7429b7e5127';
 export const LENS_BOARD_IMG_3 =
     'https://firebasestorage.googleapis.com/v0/b/library-of-lenses.appspot.com/o/images%2FLens-image-top-raw_03.gif?alt=media&token=1b82e866-dc1f-4bcc-880d-511e321e1b2f';
+
+// Popular photos used on Landing page
+export const POPULAR_PHOTOS = [
+    'https://live.staticflickr.com/7918/40638996773_c54f442d61_b.jpg',
+    'https://live.staticflickr.com/65535/52009379039_b893952e01_b.jpg',
+    'https://live.staticflickr.com/7837/33728155338_ab63081a11_b.jpg',
+    'https://live.staticflickr.com/65535/52009379409_bf3c26e521_b.jpg',
+    'https://live.staticflickr.com/7894/33728154658_e794ea52ba_b.jpg',
+    'https://live.staticflickr.com/7812/40638996323_54c45765b7_b.jpg',
+    'https://live.staticflickr.com/65535/52009648840_85f852481c_b.jpg',
+    'https://live.staticflickr.com/7912/40638995803_f2131587f4_b.jpg',
+    'https://live.staticflickr.com/65535/52009648350_738d6fe8e7_b.jpg',
+    'https://live.staticflickr.com/65535/52009649270_5b195d5cac_b.jpg',
+];
+
+// Icons for Contact section in App
+export const GITLAB_ICON =
+    'https://cdn-icons-png.flaticon.com/512/5968/5968853.png';
+export const GITHUB_ICON =
+    'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png';
+export const LINKEDIN_ICON =
+    'https://cdn-icons-png.flaticon.com/512/174/174857.png';
+
+export const LINKEDIN_PROFILE_ULR =
+    'https://www.linkedin.com/in/veskostadinov/';
+export const GITLAB_PROFILE_URL = 'https://gitlab.com/coding.Ves';
+export const GITHUB_PROFILE_URL = 'https://github.com/coding-Ves';
+
+export const NOT_FOUND_IMAGE =
+    'https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7968.jpg?w=826&t=st=1692296294~exp=1692296894~hmac=b88d5be2fddd8a98c57dd82f43c8512256999e1255e07f02acd918b27530f929';
--- src/services/flickr.service.ts
+++ src/services/flickr.service.ts
@@ -12,7 +12,9 @@ export const URLAlbumHandler = (url: string): string => {
 };
 
 export const fetchPhotoById = (photoId: string) => {
-    const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${FLICKR_API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
+    const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${
+        import.meta.env.VITE_FLICKR_API_KEY
+    }&photo_id=${photoId}&format=json&nojsoncallback=1`;
 
     const photo = fetch(apiUrl)
         .then((response) => {
@@ -30,7 +32,9 @@ export const fetchPhotoById = (photoId: string) => {
 };
 
 export const fetchGalleryById = async (galleryID: string) => {
-    const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${FLICKR_API_KEY}&photoset_id=${galleryID}&format=json&nojsoncallback=1`;
+    const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${
+        import.meta.env.VITE_FLICKR_API_KEY
+    }&photoset_id=${galleryID}&format=json&nojsoncallback=1`;
 
     try {
         const response = await fetch(apiUrl);
@@ -46,8 +50,10 @@ export const fetchGalleryById = async (galleryID: string) => {
     }
 };
 
-export const getPhotosByGallery = (gallery: Array<galleryItem>) => {
-    const galleryArray: Array<galleryItem> = [];
+export const getPhotosByGallery = (
+    gallery: Array<galleryItem>
+): Array<string> => {
+    const galleryArray: Array<string> = [];
     gallery.map((photo) => {
         fetchPhotoById(photo.id)
             .then((response) => {
@@ -59,4 +65,29 @@ export const getPhotosByGallery = (gallery: Array<galleryItem>) => {
     });
 
     return galleryArray;
+};
+
+export const getMostPopularPhotos = async () => {
+    const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.getPopular&api_key=${
+        import.meta.env.VITE_FLICKR_API_KEY
+    }&user_id=169368872%40N02&per_page=10&page=12&format=json&nojsoncallback=1`;
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Failed to fetch photos');
+        }
+
+        const data = await response.json();
+        return data.photos.photo;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to fetch photos');
+    }
+};
+
+export const getLandingGalleryPhotos = async (): Promise<Array<string>> => {
+    const popularPhotos = await getMostPopularPhotos();
+    const gallery = await getPhotosByGallery(popularPhotos);
+    return gallery;
 };
